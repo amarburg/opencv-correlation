97469c6c7fa9a859bfb88f290dd4ea5a4799d45e
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2013-01-21 20:36:53 +1300

    Just mucking about.

--- test_texture_psd.cpp
+++ test_texture_psd.cpp
@@ -18,8 +18,8 @@ static void help_and_exit( void )
   exit(-1);
 }
 
-const unsigned int PSD_WIDTH = 16;
-const unsigned int PSD_HEIGHT = PSD_WIDTH;
+const unsigned int FFT_WIDTH = 16;
+const unsigned int FFT_HEIGHT = FFT_WIDTH;
 
 
 int main( int argc, char **argv )
@@ -56,7 +56,7 @@ int main( int argc, char **argv )
   //  add_gaussian_noise( imageOne, gaussian_stddev );
  //}
 
-  unsigned int gutter = PSD_WIDTH/2;
+  unsigned int gutter = FFT_WIDTH/2;
 
   // Check image
   if( imageOne.depth() != CV_8U || imageOne.channels() != 1 ) {
@@ -64,47 +64,86 @@ int main( int argc, char **argv )
     exit(-1);
   }
 
+  unsigned int x = 100, y = 100;
   fftw_complex *data_in;
   fftw_complex *fft_out;
 
-  //data_in = (fftw_complex *)fftw_malloc( sizeof( fftw_complex ) * PSD_WIDTH * PSD_HEIGHT );
-  //fft_out = (fftw_complex *)fftw_malloc( sizeof( fftw_complex ) * PSD_WIDTH * PSD_HEIGHT );
+  data_in = (fftw_complex *)fftw_malloc( sizeof( fftw_complex ) * FFT_WIDTH * FFT_HEIGHT );
+  fft_out = (fftw_complex *)fftw_malloc( sizeof( fftw_complex ) * FFT_WIDTH * FFT_HEIGHT );
 
-  data_in = (fftw_complex *)fftw_malloc( sizeof( fftw_complex ) * PSD_WIDTH * PSD_HEIGHT );
-  fft_out = (fftw_complex *)fftw_malloc( sizeof( fftw_complex ) * PSD_WIDTH * PSD_HEIGHT );
-  
+  fftw_plan planner = fftw_plan_dft_2d( FFT_WIDTH, FFT_HEIGHT, data_in, fft_out, FFTW_FORWARD, FFTW_MEASURE );
+  fftw_print_plan( planner );
+
+// NTFS: FFTW_MEASURE overwrites the input array.  Create the planner
+// before you fill the array.
+
   // Copy image data to data_in
   int k = 0;
-  for( unsigned int i = 0; i < PSD_WIDTH; i++ ) {
-    for( unsigned int j = 0; j < PSD_WIDTH;  j++ ) {
-      data_in[k][0] = imageOne.at<unsigned char>( i, j );
+  float offset = 1.0;
+  for( unsigned int r = 0; r < FFT_HEIGHT; r++ ) {
+    for( unsigned int c = 0; c < FFT_WIDTH;  c++ ) {
+      //data_in[k][0] = 1.0 * imageOne.at<unsigned char>( x+c, y+r );
+      //
+      // By FFTW FAQ 3.11 this will offset the origin to
+      // the center of the output (aka shifting everything by
+      // Nyquist frequency
+
+      offset = ((r+c) % 2 == 0) ? -1.0 : 1.0;
+      //offset = pow( -1, r+c );
+
+      data_in[k][0] = offset * imageOne.at<unsigned char>( x+c, y+r );
       data_in[k][1] = 0.0;
       k++;
     }
   }
 
-  fftw_plan planner = fftw_plan_dft_2d( PSD_WIDTH, PSD_HEIGHT, data_in, fft_out, FFTW_FORWARD, FFTW_MEASURE );
-  fftw_print_plan( planner );
   fftw_execute( planner );
 
   // Copy data to an image and save
-  Mat data_out( PSD_WIDTH, PSD_HEIGHT, CV_32F, Scalar(0) );
-  Mat data_as_8u(PSD_WIDTH, PSD_HEIGHT, CV_8U, Scalar(0) );
+  Mat data_out( FFT_HEIGHT, FFT_WIDTH, CV_32F );
+  Mat data_as_8u(FFT_HEIGHT, FFT_WIDTH, CV_8U, Scalar(0) );
 
+  printf("\n");
   k = 0;
-  for( unsigned int i = 0; i < PSD_WIDTH; i++ ) {
-    for( unsigned int j = 0; j < PSD_WIDTH;  j++ ) {
+  for( unsigned int r = 0; r < FFT_HEIGHT; r++ ) {
+    for( unsigned int c = 0; c < FFT_WIDTH;  c++ ) {
       // Use magnitude
-      printf("%d,%d  %f,%f\n", i,j,fft_out[k][0], fft_out[k][1] );
-      data_out.at<float>(i,j) = sqrt( fft_out[k][0]*fft_out[k][0] + fft_out[k][1]*fft_out[k][1] );
+      double result = sqrt( fft_out[k][0]*fft_out[k][0] + fft_out[k][1]*fft_out[k][1] );
+      printf("%d: %d,%d  %le, %le, %le\n", k, c,r,fft_out[k][0], fft_out[k][1], result );
+      data_out.at<float>(r,c) = log( result );
       k++;
     }
   }
 
-  data_out.convertTo(  data_as_8u, CV_8U, 256, 0 );
+  // Convert float image to 8U image, scale to
+  // max dynamic range
+  double theMin, theMax;
+  minMaxLoc( data_out, &theMin, &theMax);
+
+  // Find the smallest non-NAN value
+  theMin = theMax;
+  for( unsigned int r = 0; r < FFT_HEIGHT; r++ ) {
+    for( unsigned int c = 0; c < FFT_WIDTH;  c++ ) {
+      float d = data_out.at<float>(r,c);
+      if( d != -HUGE_VAL )  
+        theMin = MIN(d, theMin);
+    }
+  }
+
+  // The patchup the NANs
+  for( unsigned int r = 0; r < FFT_HEIGHT; r++ ) {
+    for( unsigned int c = 0; c < FFT_WIDTH;  c++ ) {
+      if( data_out.at<float>(r,c) == -HUGE_VAL )
+        data_out.at<float>(r,c) = theMin;
+    }
+  }
+
+  float scale = 256/(theMax - theMin);
+
+  printf("Min = %.2e, max = %.2e, scale = %.2e\n", theMin, theMax, scale );
+
+  data_out.convertTo(  data_as_8u, CV_8U, scale, -theMin );
   imwrite("fftw.png", data_as_8u );
-
-
 
   fftw_destroy_plan( planner );
   fftw_free( data_in );
